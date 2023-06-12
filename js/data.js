c15886f706615c3d60b456e88f45cc35cddd3e44
Eli Bales <elibales21@gmail.com>
2023-06-12 19:18:46 +0000

    added initial data object and load/unload functions

--- js/data.js
+++ js/data.js
@@ -1,1 +1,22 @@
 /* exported data */
+window.addEventListener('beforeunload', event => {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('data-storage', dataJSON);
+});
+
+let data = {
+  // premierTable: null,
+  // singleTeam: null,
+  // squad: null,
+  // top5scorers: null,
+  // top5assisters: null,
+  // bundesligaTable: null,
+  // coach: null
+  tableArray: [null, null, null, null, null],
+  view: null
+};
+
+const previousDataJSON = localStorage.getItem('data-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
